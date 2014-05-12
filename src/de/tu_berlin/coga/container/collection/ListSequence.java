888d4386c12ae3e015ecc56b5ca6ca88a28fb284
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2014-05-12 18:32:24 +0000

    graph data structure cleanup 1

--- src/de/tu_berlin/coga/container/collection/ListSequence.java
+++ src/de/tu_berlin/coga/container/collection/ListSequence.java
@@ -13,10 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-/*
- * ListSequence.java
- *
- */
+
 package de.tu_berlin.coga.container.collection;
 
 import de.tu_berlin.coga.container.mapping.Identifiable;
